2a468acf03a0e57d1c2943404f4cc7c73550771f
flatironschool <danliu277@gmail.com>
2020-07-01 12:12:44 -0400

    Fix django-framework routes

--- rewild_backend_python/urls.py
+++ rewild_backend_python/urls.py
@@ -17,11 +17,11 @@ from django.urls import include, path
 from rest_framework import routers
 from quickstart import views
 
-# router = routers.DefaultRouter()
-# router.register('users', views.UserViewSet)
+router = routers.DefaultRouter()
+router.register('users', views.UserViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
