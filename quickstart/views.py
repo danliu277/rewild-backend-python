9b2b4088a7359d82d47813ceec2bd7b02d4e1349
flatironschool <danliu277@gmail.com>
2020-07-01 12:02:30 -0400

    Setup basic route

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+# from rewild_backend_python.quickstart.serializers import UserSerializer
+from django.http import HttpResponse
 
-# Create your views here.
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all().order_by('-date_joined')
+#     serializer_class = UserSerializer
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
