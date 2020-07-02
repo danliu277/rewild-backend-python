727b5c8d886310c40a89a4f686c9b3cc86ff7ff5
flatironschool <danliu277@gmail.com>
2020-07-02 16:42:58 -0400

    Restart project

--- api_example/languages/apps.py
+++ api_example/languages/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class MyapiConfig(AppConfig):
-    name = 'myapi'
+class LanguagesConfig(AppConfig):
+    name = 'languages'
